db2a5cf5ccaf85c75d4af0468a367f199fa1a3b2
FN03A078 <825281732@qq.com>
2022-10-11 17:26:55 +0800

    push test

--- com/shan/BackTrack/Git.java
+++ com/shan/BackTrack/Git.java
@@ -10,5 +10,8 @@ public class Git {
         System.out.println("hello Git!");
         System.out.println("hello Git again!");
         System.out.println("hello Git again!!!!!");
+        System.out.println("hello Git again!!!!!");
+        System.out.println("hello Git again!!!!!");
+        System.out.println("push test");
     }
 }
